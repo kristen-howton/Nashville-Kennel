1ccbc68cda36acbb3433ae8a3670f76885c90576
kristen-howton <kristen.howton@gmail.com>
2020-04-21 18:08:34 -0500

    set up employee, customer, animal, need to set up map later

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,12 +1,18 @@
 import React from "react"
-import Animal from "./animal/Animal"
 import "./animal/Animal.css"
 import Employee from "./employee/Employee"
 import "./employee/Employee.css"
 import Customer from "./customer/Customer"
 import "./customer/Customer.css"
-import Location from "./location/Location"
 import "./location/Location.css"
+import { LocationProvider } from "./location/LocationProvider"
+import  LocationList  from "./location/LocationList"
+import { AnimalProvider } from "./animal/AnimalProvider"
+import AnimalList from "./animal/AnimalList"
+import { CustomerProvider } from "./customer/CustomerProvider"
+import CustomerList from "./customer/CustomerList"
+import { EmployeeProvider } from "./employee/EmployeeProvider"
+import EmployeeList from "./employee/EmployeeList"
 
 export default () => (
     <>
@@ -19,29 +25,20 @@ export default () => (
         </address>
 
         <h2>Animals</h2>
-        <article className="animals">
-            <Animal />
-            <Animal />
-            <Animal />
-        </article>
+            <AnimalProvider>
+                <AnimalList />
+            </AnimalProvider>
         <h2>Employees</h2>
-        <article className="employees">
-            <Employee />
-            <Employee />
-            <Employee />
-        </article>
+            <EmployeeProvider>
+                <EmployeeList />
+            </EmployeeProvider>
         <h2>Locations</h2>
-        <article className="locations">
             <LocationProvider>
                 <LocationList />
             </LocationProvider>
-        </article>
         <h2>Customers</h2>
-        <article className="customers">
-            <Customer />
-            <Customer />
-            <Customer />
-            <Customer />
-        </article>
+        <CustomerProvider>
+            <CustomerList />
+        </CustomerProvider>
     </>
 )
