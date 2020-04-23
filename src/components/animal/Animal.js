d1fb5118c8b8f0fc26476b9acfb9055dea365961
kristen-howton <kristen.howton@gmail.com>
2020-04-23 09:22:58 -0500

    now it displays names instead of numbers on AnimalList and CustomerList

--- src/components/animal/Animal.js
+++ src/components/animal/Animal.js
@@ -4,7 +4,7 @@ export default ({animal, location, customer}) => (
     <section className="animal">
         <h3 className="animal__name">{animal.name}</h3>
         <div className="animal__breed">{animal.breed}</div>
-        {/* <div className="animal__location">{location.name}</div>
-        <div className="animal__owner">{customer.name}</div> */}
+        <div className="animal__location">{location.name}</div>
+        <div className="animal__owner">{customer.name}</div>
     </section>
 )
